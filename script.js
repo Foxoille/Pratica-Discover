a166184ce242ecd50b3833fc4a65d8f5a0fab984
“Eduardo_Miglioranza” <eduardomilhoranza@gmail.com>
2025-05-25 21:40:20 +0200

    step 1 animation button

--- script.js
+++ script.js
@@ -2,16 +2,19 @@ let idiomaAtual = "pt"
 
 function languageMode() {
   const textos = document.querySelectorAll(".texto")
+  const html = document.documentElement
 
   if (idiomaAtual === "pt") {
     idiomaAtual = "fr"
     textos.forEach((el) => {
       el.textContent = el.getAttribute("data-" + idiomaAtual)
     })
+    html.classList.add("frenchStyle")
   } else {
     idiomaAtual = "pt"
     textos.forEach((el) => {
       el.textContent = el.getAttribute("data-" + idiomaAtual)
     })
+    html.classList.remove("frenchStyle")
   }
 }
